ba0cd1c4c0809697f3e00bde92515ef699de6be7
sxl <sunxilin@agora.io>
2023-12-14 15:52:20 +0800

    fix

--- app/utils.py
+++ app/utils.py
@@ -12,6 +12,8 @@ filename = re.findall('(.*).py', os.path.basename(__file__))
 #Write stderr to log file.
 def errorLog(pkg_path,filename):
     import sys
+    if not os.path.exists('./'+ pkg_path +'_log_/'):
+        os.makedirs('./'+ pkg_path +'_log_/')
     sys.stderr = open('./'+ pkg_path +'_log_/' + filename[0] + '_stderr.txt', 'w')  # redirect stderr to file
 
 
@@ -29,6 +31,8 @@ def printoutHeader():
 
 
 def exceptionLog(pkg_path,filename,func_name,error,loop_item): #230303update
+    if not os.path.exists('./'+ pkg_path +'_log_/'):
+        os.makedirs('./'+ pkg_path +'_log_/')
     with open('./'+ pkg_path +'_log_/' + filename[0] + '_exceptions.txt', 'a') as f:
         # write the exception details to the file
         f.write(printoutHeader() + 'Caught exception in ' + func_name + ' during loop of ' + loop_item + ':' + '\n' + str(error) + '\n')
